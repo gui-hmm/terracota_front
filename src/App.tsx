17f6b3cb2d3b8b5c23115371515aa29afd177d86
gui-hmm <henriquemelomoura@gmail.com>
2024-11-12 09:23:26 -0300

    adicionando rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css';
 import Home from './pages/home/home'
 import QuemSomos from './pages/quemSomos/quemSomos';
@@ -9,11 +10,15 @@ import Cadastro from './pages/cadastro/cadastro';
 function App() {
   return (
     <div className="App">
-      <Home/>
-      {/* <QuemSomos/> */}
-      {/* <Produtos/> */}
-      {/* <Login/> */}
-      {/* <Cadastro/> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/quemsomos' element={<QuemSomos/>} />
+          <Route path='/produtos' element={<Produtos/>} />
+          <Route path='/login' element={<Login/>} />
+          <Route path='/cadastro' element={<Cadastro/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
